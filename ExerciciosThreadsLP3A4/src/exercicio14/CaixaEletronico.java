5c25df87d68604b06a87bfebc25f9fb5aac8f5fe
Fabio Mendes <fabiomendes53@gmail.com>
2021-06-11 13:21:48 -0300

    Feita a correção no exercício 14

--- ExerciciosThreadsLP3A4/src/exercicio14/CaixaEletronico.java
+++ ExerciciosThreadsLP3A4/src/exercicio14/CaixaEletronico.java
@@ -1,10 +1,38 @@
 package exercicio14;
 
+import static java.lang.Thread.sleep;
+
 public class CaixaEletronico {
 	
 	private Integer totalCedulas = 300;
+	public static boolean caixaVazio = false;
 	
 	public synchronized void operacao(String cliente, Boolean saque, Boolean transferencia) {
+		
+		if( transferencia ) {
+			
+			if(!caixaVazio) {
+				try {
+		            Thread.sleep(10000);
+		            totalCedulas = 500;
+		        } catch (InterruptedException e) {
+		            e.printStackTrace();
+		        }
+			}
+
+	        caixaVazio = false;
+	        notifyAll();
+			System.out.println("Cliente " + cliente + " iniciou operacao de transferencia!");
+			for(int aux=0; aux < 5; aux++) {
+				try {
+					System.out.println("[" + cliente + "] Realizando transferencia...");
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 		
 		if( saque ) {
 			System.out.println("Cliente " + cliente + " iniciou operacao de saque!");
@@ -20,9 +48,10 @@ public class CaixaEletronico {
 				
 				// Verificar se há cédulas disponíveis...
 				if( totalCedulas <= 0 ) {
+					caixaVazio = true;
 					System.out.println("Acabaram as cedulas!");
 					
-					Cedulas c = new Cedulas();
+					/*Cedulas c = new Cedulas();
 					Thread t = new Thread(c);
 					c.abastecer(t);
 					
@@ -31,24 +60,19 @@ public class CaixaEletronico {
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-					}
+					}*/
+					
+					try {
+			    		while(caixaVazio)
+			    			wait();
+			    		sleep(8000);
+			        } catch (InterruptedException e){
+			            e.printStackTrace();
+			        }
 					
 					totalCedulas = 500;
 
 					System.out.println("Novas cedulas inseridas!");
-				}
-			}
-		}
-		
-		if( transferencia ) {
-			System.out.println("Cliente " + cliente + " iniciou operacao de transferencia!");
-			for(int aux=0; aux < 5; aux++) {
-				try {
-					System.out.println("[" + cliente + "] Realizando transferencia...");
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
 				}
 			}
 		}
